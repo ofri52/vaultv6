a1ca2102cd6c7b82c636dbeac6c5d3d4f6b401a3
AJ H <105573909+1ajh@users.noreply.github.com>
2025-02-03 15:58:57 +0000

    sigma config

--- home/uv.config.js
+++ home/uv.config.js
@@ -1,16 +1,16 @@
 self.__uv$config = {
-    prefix: '/home/tiw/',
+    prefix: "/home/tiw/",
     bare: [
-        'https://bare.benrogo.net',
-        'https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/', // sorry, but also not sorry
-        'https://tomp.app',
-        'https://immortaltowillose.xyz/bare/',
-        'https://moonlight.silvereen.net/bare/' // sorry...
+        "https://bare.benrogo.net",
+        "https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/",
+        "https://tomp.app",
+        "https://immortaltowillose.xyz/bare/",
+        "https://moonlight.silvereen.net/bare/"
     ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/home/uv/uv.handler.js',
-    bundle: '/home/uv/uv.bundle.js',
-    config: '/home/uv/uv.config.js',
-    sw: '/home/uv/uv.sw.js',
+    handler: "/home/uv/uv.handler.js",
+    bundle: "/home/uv/uv.bundle.js",
+    config: "/home/uv/uv.config.js",
+    sw: "/home/uv/uv.sw.js"
 };
