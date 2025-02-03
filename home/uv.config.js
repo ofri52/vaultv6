10f331d216d15e25a7cbf317af0585b887cee972
AJ H <105573909+1ajh@users.noreply.github.com>
2025-02-03 15:56:18 +0000

    bare fix2

--- home/uv.config.js
+++ home/uv.config.js
@@ -2,10 +2,10 @@ self.__uv$config = {
     prefix: '/home/tiw/',
     bare: [
         'https://bare.benrogo.net',
-        'https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/' // sorry, but also not sorry
+        'https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/', // sorry, but also not sorry
         'https://tomp.app',
         'https://immortaltowillose.xyz/bare/',
-        'https://moonlight.silvereen.net/bare/', // sorry...
+        'https://moonlight.silvereen.net/bare/' // sorry...
     ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
