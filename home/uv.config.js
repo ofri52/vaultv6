e8c1e5d80327f815073f70ca6159ab6ef3ee6de1
AJ H <105573909+1ajh@users.noreply.github.com>
2025-02-03 16:11:15 +0000

    more bare fallbacks

--- home/uv.config.js
+++ home/uv.config.js
@@ -1,16 +1,20 @@
 self.__uv$config = {
-    prefix: "/home/tiw/",
+    prefix: '/home/tiw/',
     bare: [
-        "https://bare.benrogo.net",
-        "https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/",
-        "https://tomp.app",
-        "https://immortaltowillose.xyz/bare/",
-        "https://moonlight.silvereen.net/bare/"
+        'https://bare.benrogo.net',
+        'https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/',
+        'https://tomp.app',
+        'https://void.radio.fm/bare/',
+        'https://0ziuro-6969.csb.app/',
+        'https://arse.masplenedigitalworld.com/bare/',
+        'https://immortaltowillose.xyz/bare/',
+        'https://backend.infrared.bomberfish.ca',
+        'https://moonlight.silvereen.net/bare/'
     ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/home/uv/uv.handler.js",
-    bundle: "/home/uv/uv.bundle.js",
-    config: "/home/uv/uv.config.js",
-    sw: "/home/uv/uv.sw.js"
+    handler: '/home/uv/uv.handler.js',
+    bundle: '/home/uv/uv.bundle.js',
+    config: '/home/uv/uv.config.js',
+    sw: '/home/uv/uv.sw.js'
 };
