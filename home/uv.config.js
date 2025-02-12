4b42e92c7fd210f27fd7411b34b69d0d409b0bd7
AJ H <105573909+1ajh@users.noreply.github.com>
2025-02-12 14:59:29 +0000

    bare update

--- home/uv.config.js
+++ home/uv.config.js
@@ -1,21 +1,6 @@
 self.__uv$config = {
     prefix: '/home/tiw/',
-    bare: [
-        'https://mexi.rest/bare/',
-        'https://mexi.jobi.one/bare/',
-        'https://bare.benrogo.net',
-        'https://meximathbarepacketyo.global.ssl.fastly.net/bare/',
-        'https://rizzingupgyattsinohiofanumtaxxinguncskibiditoiletxdlmfaookbye.v2202412246404304352.megasrv.de/bare/',
-        'https://tomp.app',
-        'https://thesizeoftheworldismassive.global.ssl.fastly.net/',
-        'https://meximathurbanned.global.ssl.fastly.net/bare/',
-        'https://sizeofthe.world/bare/',
-        'https://void.radio.fm/bare/',
-        'https://arse.masplenedigitalworld.com/bare/',
-        'https://immortaltowillose.xyz/bare/',
-        'https://backend.infrared.bomberfish.ca',
-        'https://moonlight.silvereen.net/bare/'
-    ],
+    bare: 'https://funfactsaboutmexico.ajh.college/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/home/uv/uv.handler.js',
