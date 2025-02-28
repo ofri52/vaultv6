968ad64534f4d0010b62c851b6f7235ced33542c
Aidan H <105573909+1ajh@users.noreply.github.com>
2025-02-28 13:59:15 -0500

    Update uv.config.js

--- home/uv/uv.config.js
+++ home/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/home/tiw/',
-    bare:'https://bare.benrogo.net',
+    prefix: '/home/',
+    bare: 'https://animal-bear.ajh.boats/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/home/uv/uv.handler.js',
     bundle: '/home/uv/uv.bundle.js',
     config: '/home/uv/uv.config.js',
-    sw: '/home/uv/uv.sw.js',
+    sw: '/home/uv/uv.sw.js'
 };
