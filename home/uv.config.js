55187d489b9e34c0f2d5d56f9cc32628263c79b6
Aidan H <105573909+1ajh@users.noreply.github.com>
2025-02-28 13:53:53 -0500

    Update uv.config.js

--- home/uv.config.js
+++ home/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/home/tiw/',
+    prefix: '/home/',
     bare: 'https://animal-bear.ajh.boats/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
